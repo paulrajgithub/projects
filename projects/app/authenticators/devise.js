9b3883e72b6358b3156f2eef8f8d777c5679300c
Jamiu Adeleke <adet4ever@gmail.com>
2016-06-13 22:33:38 -0230

    dasfas

--- projects/app/authenticators/devise.js
+++
@@ -1,6 +0,0 @@
-import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
-
-export default DeviseAuthenticator.extend({
-	serverTokenEndpoint: 'http://localhost:3000/users/sign_in'
-
-});
--- projects/app/controllers/answer.js
+++
@@ -1,4 +0,0 @@
-// app/controllers/answer.js
-import Ember from "ember";
-
-export default Ember.ObjectController.extend({});
